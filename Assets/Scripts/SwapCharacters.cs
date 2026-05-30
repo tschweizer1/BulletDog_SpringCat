585e35bce68c237cb84a0ff28e97f79879e6f412
Tyler Schweizer <tschweizer@chapman.edu>
2026-05-29 19:45:08 -0700

    Created Variants and ability to swap character with momentum conserved

---
+++ Assets/Scripts/SwapCharacters.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SwapCharacters : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private InputAction SwapInput;
+    public List<GameObject> Characters;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    public void OnSwapChar1()
+    {
+        SwapCharacter(1);
+    }
+    
+    public void OnSwapChar2()
+    {
+        SwapCharacter(2);
+    }
+    
+    public void OnSwapChar3()
+    {
+        SwapCharacter(3);
+    }
+    
+    public void OnSwapChar4()
+    {
+        SwapCharacter(4);
+    }
+
+    private void SwapCharacter(int animal)
+    {
+        GameObject swappedAnimal = Instantiate(Characters[animal-1], gameObject.transform.position, Quaternion.identity);
+        swappedAnimal.GetComponent<Rigidbody2D>().AddForce(gameObject.GetComponent<Rigidbody2D>().linearVelocity, ForceMode2D.Impulse);
+        Destroy(gameObject);
+    }
+}
